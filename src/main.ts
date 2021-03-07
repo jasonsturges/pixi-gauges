694d4759d0445c166fddce0d41f00d4d9aaa4c80
Jason Sturges <jason@jasonsturges.com>
2021-03-07 15:42:46 -0600

    Main exports

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-export { AbstractGauge } from "./AbstractGauge";
+export { AbstractAxis } from "./AbstractAxis";
+export { AbstractControl } from "./AbstractControl";
+export { IAxis } from "./IAxis";
+export { IControl } from "./IControl";
+export { IPolarAxis } from "./IPolarAxis";
+export { PolarAxis } from "./PolarAxis";
