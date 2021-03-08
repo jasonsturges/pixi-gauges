191137340de14e5c96f0d43d945798dfc4a42185
Jason Sturges <jason@jasonsturges.com>
2021-03-08 01:34:17 -0600

    Duplicate construction

--- src/AbstractControl.ts
+++ src/AbstractControl.ts
@@ -3,12 +3,4 @@ import { IControl } from "./IControl";
 
 export abstract class AbstractControl extends PIXI.Sprite implements IControl {
   _dirty: boolean = true;
-
-  /**
-   * @constructor
-   */
-  constructor() {
-    super();
-    this._dirty = true;
-  }
 }
