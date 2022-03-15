a3dc27f95f868945b52502e5617c0db778699ca8
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:05:18 -0500

    Needle

--- src/display/Needle.ts
+++ src/display/Needle.ts
@@ -1,97 +1,75 @@
-import * as PIXI from "pixi.js";
 import { AbstractNeedle } from "./AbstractNeedle";
+import { Graphics, ILineStyleOptions, Point } from "pixi.js";
+import { NeedleDefaultOptions } from "../factory/NeedleDefaultOptions";
 import { NeedleOptions } from "./NeedleOptions";
 
 export class Needle extends AbstractNeedle {
-  private readonly _graphics: PIXI.Graphics;
+  private readonly _graphics: Graphics | undefined;
 
   /**
-   * @constructor
+   * Constructor
    */
-  constructor(options: Partial<NeedleOptions> = {}) {
-    super();
+  constructor(options?: Partial<NeedleOptions>) {
+    super({
+      ...NeedleDefaultOptions,
+      ...options,
+    });
 
-    this.set(options);
-
-    this._graphics = new PIXI.Graphics();
+    this._graphics = new Graphics();
     this.addChild(this._graphics);
   }
 
   /**
-   * @override
-   * @param renderer
+   * Validate
    */
-  public render(renderer: PIXI.Renderer) {
-    super.render(renderer);
+  protected override validate() {
+    super.validate();
+    this._graphics?.clear();
 
-    if (!this._dirty) return;
-    this._dirty = false;
-    this._graphics.clear();
+    this._graphics?.lineStyle(<ILineStyleOptions>{
+      width: this.needleStrokeWeight,
+      color: this.needleStrokeColor,
+      alpha: this.needleStrokeAlpha,
+    });
 
-    const {
-      _graphics,
-      needleBaseWidth,
-      needleFillAlpha,
-      needleFillColor,
-      needleRadius,
-      needleStrokeAlpha,
-      needleStrokeColor,
-      needleStrokeWeight,
-      needleTipWidth,
-    } = this;
+    this._graphics?.beginFill(this.needleFillColor, this.needleFillAlpha);
 
-    _graphics.lineStyle(needleStrokeWeight, needleStrokeColor, needleStrokeAlpha);
-    _graphics.beginFill(needleFillColor, needleFillAlpha);
+    const base: number = this.needleBaseWidth * 0.5;
+    const basePoint1: Point = new Point(0, -base);
+    const basePoint2: Point = new Point(0, base);
+    const tip: number = this.needleTipWidth * 0.5;
+    const tipPoint1: Point = new Point(this.needleRadius, -tip);
+    const tipPoint2: Point = new Point(this.needleRadius, tip);
 
-    const base: number = needleBaseWidth * 0.5;
-    const basePoint1: PIXI.Point = new PIXI.Point(0, -base);
-    const basePoint2: PIXI.Point = new PIXI.Point(0, base);
-    const tip: number = needleTipWidth * 0.5;
-    const tipPoint1: PIXI.Point = new PIXI.Point(needleRadius, -tip);
-    const tipPoint2: PIXI.Point = new PIXI.Point(needleRadius, tip);
+    this._graphics?.moveTo(basePoint1.x, basePoint1.y);
+    this._graphics?.lineTo(tipPoint1.x, tipPoint1.y);
 
-    _graphics.moveTo(basePoint1.x, basePoint1.y);
-    _graphics.lineTo(tipPoint1.x, tipPoint1.y);
-
-    _graphics.quadraticCurveTo(
-      tipPoint1.x + needleTipWidth * 0.5,
+    this._graphics?.quadraticCurveTo(
+      tipPoint1.x + this.needleTipWidth * 0.5,
       tipPoint1.y,
-      tipPoint1.x + needleTipWidth * 0.5,
-      tipPoint1.y + needleTipWidth * 0.5
+      tipPoint1.x + this.needleTipWidth * 0.5,
+      tipPoint1.y + this.needleTipWidth * 0.5,
     );
 
-    _graphics.quadraticCurveTo(
-      tipPoint2.x + needleTipWidth * 0.5,
-      tipPoint2.y,
-      tipPoint2.x,
-      tipPoint2.y
-    );
+    this._graphics?.quadraticCurveTo(tipPoint2.x + this.needleTipWidth * 0.5, tipPoint2.y, tipPoint2.x, tipPoint2.y);
 
-    _graphics.lineTo(basePoint2.x, basePoint2.y);
+    this._graphics?.lineTo(basePoint2.x, basePoint2.y);
 
-    _graphics.quadraticCurveTo(
-      basePoint2.x - needleBaseWidth * 0.5,
+    this._graphics?.quadraticCurveTo(
+      basePoint2.x - this.needleBaseWidth * 0.5,
       basePoint2.y,
-      basePoint2.x - needleBaseWidth * 0.5,
-      basePoint2.y - needleBaseWidth * 0.5
+      basePoint2.x - this.needleBaseWidth * 0.5,
+      basePoint2.y - this.needleBaseWidth * 0.5,
     );
 
-    _graphics.quadraticCurveTo(
-      basePoint1.x - needleBaseWidth * 0.5,
+    this._graphics?.quadraticCurveTo(
+      basePoint1.x - this.needleBaseWidth * 0.5,
       basePoint1.y,
       basePoint1.x,
-      basePoint1.y
+      basePoint1.y,
     );
 
-    _graphics.endFill();
-  }
-
-  /**
-   * Apply needle options
-   * @param {NeedleOptions} options - Options to be applied
-   */
-  public set(options: Partial<NeedleOptions> = {}) {
-    Object.assign(this, options);
-    this._dirty = true;
+    this._graphics?.endFill();
+    this._graphics?.closePath();
   }
 }
