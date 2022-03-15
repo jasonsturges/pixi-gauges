60c28aac407437cfa46ce302ad8b3c0864a96133
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:13:47 -0500

    Abstract polar axis

--- src/display/AbstractPolarAxis.ts
+++ src/display/AbstractPolarAxis.ts
@@ -1,10 +1,43 @@
 import { AbstractAxis } from "./AbstractAxis";
 import { IPolarAxis } from "./IPolarAxis";
+import { PolarAxisOptions } from "./PolarAxisOptions";
 
 export abstract class AbstractPolarAxis extends AbstractAxis implements IPolarAxis {
-  protected _radius: number = 100;
-  protected _spanAngle: number = 260;
-  protected _startAngle: number = 140;
+  private _radius: number | undefined;
+  private _spanAngle: number | undefined;
+  private _startAngle: number | undefined;
+
+  protected constructor(options: Partial<PolarAxisOptions>) {
+    super({
+      axisAlpha: options?.axisAlpha,
+      axisColor: options?.axisColor,
+      axisLabelFontAlpha: options?.axisLabelFontAlpha,
+      axisLabelFontColor: options?.axisLabelFontColor,
+      axisLabelFontName: options?.axisLabelFontName,
+      axisLabelFontSize: options?.axisLabelFontSize,
+      axisLabelGap: options?.axisLabelGap,
+      axisLabelsVisible: options?.axisLabelsVisible,
+      axisVisible: options?.axisVisible,
+      axisWeight: options?.axisWeight,
+      majorTickAlpha: options?.majorTickAlpha,
+      majorTickColor: options?.majorTickColor,
+      majorTickCount: options?.majorTickCount,
+      majorTickLength: options?.majorTickLength,
+      majorTickWeight: options?.majorTickWeight,
+      maximum: options?.maximum,
+      minimum: options?.minimum,
+      minorTickAlpha: options?.minorTickAlpha,
+      minorTickColor: options?.minorTickColor,
+      minorTickCount: options?.minorTickCount,
+      minorTickLength: options?.minorTickLength,
+      minorTickWeight: options?.minorTickWeight,
+      precision: options?.precision,
+    });
+
+    this.radius = options?.radius;
+    this.spanAngle = options?.spanAngle;
+    this.startAngle = options?.startAngle;
+  }
 
   //------------------------------
   //  properties
@@ -13,42 +46,39 @@ export abstract class AbstractPolarAxis extends AbstractAxis implements IPolarAx
   /**
    * radius
    */
-  get radius(): number {
+  get radius() {
     return this._radius;
   }
 
-  set radius(value: number) {
+  set radius(value) {
     if (this._radius === value) return;
-
     this._radius = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * spanAngle
    */
-  get spanAngle(): number {
+  get spanAngle() {
     return this._spanAngle;
   }
 
-  set spanAngle(value: number) {
+  set spanAngle(value) {
     if (this._spanAngle === value) return;
-
     this._spanAngle = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * startAngle
    */
-  get startAngle(): number {
+  get startAngle() {
     return this._startAngle;
   }
 
-  set startAngle(value: number) {
+  set startAngle(value) {
     if (this._startAngle === value) return;
-
     this._startAngle = value;
-    this._dirty = true;
+    this.invalidate();
   }
 }
