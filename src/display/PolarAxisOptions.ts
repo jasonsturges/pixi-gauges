f7d189a50c4a5779fa87a4c8c7d541a30f09e37f
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:13:20 -0500

    Polar axis options

--- src/display/PolarAxisOptions.ts
+++ src/display/PolarAxisOptions.ts
@@ -1,31 +1,10 @@
+import { AxisOptions } from "./AxisOptions";
+
 /**
  * Polar Axis optional parameters
  */
-export type PolarAxisOptions = {
-  axisAlpha: number;
-  axisColor: number;
-  axisLabelFontAlpha: number;
-  axisLabelFontColor: number;
-  axisLabelFontName: string;
-  axisLabelFontSize: number;
-  axisLabelGap: number;
-  axisLabelsVisible: boolean;
-  axisVisible: boolean;
-  axisWeight: number;
-  majorTickAlpha: number;
-  majorTickColor: number;
-  majorTickCount: number;
-  majorTickLength: number;
-  majorTickWeight: number;
-  maximum: number;
-  minimum: number;
-  minorTickAlpha: number;
-  minorTickColor: number;
-  minorTickCount: number;
-  minorTickLength: number;
-  minorTickWeight: number;
-  precision: number;
-  radius: number;
-  spanAngle: number;
-  startAngle: number;
+export type PolarAxisOptions = AxisOptions & {
+  radius?: number | undefined;
+  spanAngle?: number | undefined;
+  startAngle?: number | undefined;
 };
