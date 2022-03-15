3bc399a772d25ec9dfbfd90426b945a8d05abadd
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:06:14 -0500

    Axis options

---
+++ src/display/AxisOptions.ts
@@ -0,0 +1,28 @@
+/**
+ * Axis optional parameters
+ */
+export type AxisOptions = {
+  axisAlpha?: number | undefined;
+  axisColor?: number | undefined;
+  axisLabelFontAlpha?: number | undefined;
+  axisLabelFontColor?: number | undefined;
+  axisLabelFontName?: string | undefined;
+  axisLabelFontSize?: number | undefined;
+  axisLabelGap?: number | undefined;
+  axisLabelsVisible?: boolean | undefined;
+  axisVisible?: boolean | undefined;
+  axisWeight?: number | undefined;
+  majorTickAlpha?: number | undefined;
+  majorTickColor?: number | undefined;
+  majorTickCount?: number | undefined;
+  majorTickLength?: number | undefined;
+  majorTickWeight?: number | undefined;
+  maximum?: number | undefined;
+  minimum?: number | undefined;
+  minorTickAlpha?: number | undefined;
+  minorTickColor?: number | undefined;
+  minorTickCount?: number | undefined;
+  minorTickLength?: number | undefined;
+  minorTickWeight?: number | undefined;
+  precision?: number | undefined;
+};
