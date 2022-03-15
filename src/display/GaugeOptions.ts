600654c5441588748e0d9c51f6d171ec8046abf1
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:29:40 -0500

    Gauge options

---
+++ src/display/GaugeOptions.ts
@@ -0,0 +1,9 @@
+/**
+ * Gauge optional parameters
+ */
+export type GaugeOptions = {
+  autoAdjustMinMax?: boolean | undefined;
+  maximum?: number | undefined;
+  minimum?: number | undefined;
+  value?: number | undefined;
+};
