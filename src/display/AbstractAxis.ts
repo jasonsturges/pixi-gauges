6495a81286cfaadc3f9e3c4c36d87073c14cae02
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:09:07 -0500

    Abstract axis

--- src/display/AbstractAxis.ts
+++ src/display/AbstractAxis.ts
@@ -1,30 +1,59 @@
 import { AbstractControl } from "./AbstractControl";
+import { AxisOptions } from "./AxisOptions";
 import { IAxis } from "./IAxis";
 
 export abstract class AbstractAxis extends AbstractControl implements IAxis {
-  protected _axisAlpha: number = 1;
-  protected _axisColor: number = 0x313131;
-  protected _axisLabelFontAlpha: number = 1;
-  protected _axisLabelFontColor: number = 0x313131;
-  protected _axisLabelFontName: string = "Arial";
-  protected _axisLabelFontSize: number = 8;
-  protected _axisLabelGap: number = 0.19;
-  protected _axisLabelsVisible: boolean = true;
-  protected _axisVisible: boolean = true;
-  protected _axisWeight: number = 2;
-  protected _majorTickAlpha: number = 1;
-  protected _majorTickColor: number = 0x313131;
-  protected _majorTickCount: number = 16;
-  protected _majorTickLength: number = 0.1;
-  protected _majorTickWeight: number = 2;
-  protected _maximum: number = 100;
-  protected _minimum: number = 0;
-  protected _minorTickAlpha: number = 1;
-  protected _minorTickColor: number = 0x313131;
-  protected _minorTickCount: number = 4;
-  protected _minorTickLength: number = 0.05;
-  protected _minorTickWeight: number = 1;
-  protected _precision: number = 0;
+  private _axisAlpha: number | undefined;
+  private _axisColor: number | undefined;
+  private _axisLabelFontAlpha: number | undefined;
+  private _axisLabelFontColor: number | undefined;
+  private _axisLabelFontName: string | undefined;
+  private _axisLabelFontSize: number | undefined;
+  private _axisLabelGap: number | undefined;
+  private _axisLabelsVisible: boolean | undefined;
+  private _axisVisible: boolean | undefined;
+  private _axisWeight: number | undefined;
+  private _majorTickAlpha: number | undefined;
+  private _majorTickColor: number | undefined;
+  private _majorTickCount: number | undefined;
+  private _majorTickLength: number | undefined;
+  private _majorTickWeight: number | undefined;
+  private _maximum: number | undefined;
+  private _minimum: number | undefined;
+  private _minorTickAlpha: number | undefined;
+  private _minorTickColor: number | undefined;
+  private _minorTickCount: number | undefined;
+  private _minorTickLength: number | undefined;
+  private _minorTickWeight: number | undefined;
+  private _precision: number | undefined;
+
+  protected constructor(options?: Partial<AxisOptions>) {
+    super();
+
+    this.axisAlpha = options?.axisAlpha;
+    this.axisColor = options?.axisColor;
+    this.axisLabelFontAlpha = options?.axisLabelFontAlpha;
+    this.axisLabelFontColor = options?.axisLabelFontColor;
+    this.axisLabelFontName = options?.axisLabelFontName;
+    this.axisLabelFontSize = options?.axisLabelFontSize;
+    this.axisLabelGap = options?.axisLabelGap;
+    this.axisLabelsVisible = options?.axisLabelsVisible;
+    this.axisVisible = options?.axisVisible;
+    this.axisWeight = options?.axisWeight;
+    this.majorTickAlpha = options?.majorTickAlpha;
+    this.majorTickColor = options?.majorTickColor;
+    this.majorTickCount = options?.majorTickCount;
+    this.majorTickLength = options?.majorTickLength;
+    this.majorTickWeight = options?.majorTickWeight;
+    this.maximum = options?.maximum;
+    this.minimum = options?.minimum;
+    this.minorTickAlpha = options?.minorTickAlpha;
+    this.minorTickColor = options?.minorTickColor;
+    this.minorTickCount = options?.minorTickCount;
+    this.minorTickLength = options?.minorTickLength;
+    this.minorTickWeight = options?.minorTickWeight;
+    this.precision = options?.precision;
+  }
 
   //------------------------------
   //  properties
@@ -33,330 +62,299 @@ export abstract class AbstractAxis extends AbstractControl implements IAxis {
   /**
    * axisAlpha
    */
-  get axisAlpha(): number {
+  get axisAlpha() {
     return this._axisAlpha;
   }
 
-  set axisAlpha(value: number) {
+  set axisAlpha(value) {
     if (this._axisAlpha === value) return;
-
     this._axisAlpha = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisColor
    */
-  get axisColor(): number {
+  get axisColor() {
     return this._axisColor;
   }
 
-  set axisColor(value: number) {
+  set axisColor(value) {
     if (this._axisColor === value) return;
-
     this._axisColor = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisLabelFontAlpha
    */
-  get axisLabelFontAlpha(): number {
+  get axisLabelFontAlpha() {
     return this._axisLabelFontAlpha;
   }
 
-  set axisLabelFontAlpha(value: number) {
+  set axisLabelFontAlpha(value) {
     if (this._axisLabelFontAlpha === value) return;
-
     this._axisLabelFontAlpha = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisLabelFontColor
    */
-  get axisLabelFontColor(): number {
+  get axisLabelFontColor() {
     return this._axisLabelFontColor;
   }
 
-  set axisLabelFontColor(value: number) {
+  set axisLabelFontColor(value) {
     if (this._axisLabelFontColor === value) return;
-
     this._axisLabelFontColor = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisLabelFontName
    */
-  get axisLabelFontName(): string {
+  get axisLabelFontName() {
     return this._axisLabelFontName;
   }
 
-  set axisLabelFontName(value: string) {
+  set axisLabelFontName(value) {
     if (this._axisLabelFontName === value) return;
-
     this._axisLabelFontName = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisLabelFontSize
    */
-  get axisLabelFontSize(): number {
+  get axisLabelFontSize() {
     return this._axisLabelFontSize;
   }
 
-  set axisLabelFontSize(value: number) {
+  set axisLabelFontSize(value) {
     if (this._axisLabelFontSize === value) return;
-
     this._axisLabelFontSize = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisLabelGap
    */
-  get axisLabelGap(): number {
+  get axisLabelGap() {
     return this._axisLabelGap;
   }
 
-  set axisLabelGap(value: number) {
+  set axisLabelGap(value) {
     if (this._axisLabelGap === value) return;
-
     this._axisLabelGap = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisLabelsVisible
    */
-  get axisLabelsVisible(): boolean {
+  get axisLabelsVisible() {
     return this._axisLabelsVisible;
   }
 
-  set axisLabelsVisible(value: boolean) {
+  set axisLabelsVisible(value) {
     if (this._axisLabelsVisible === value) return;
-
     this._axisLabelsVisible = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisVisible
    */
-  get axisVisible(): boolean {
+  get axisVisible() {
     return this._axisVisible;
   }
 
-  set axisVisible(value: boolean) {
+  set axisVisible(value) {
     if (this._axisVisible === value) return;
-
     this._axisVisible = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * axisWeight
    */
-  get axisWeight(): number {
+  get axisWeight() {
     return this._axisWeight;
   }
 
-  set axisWeight(value: number) {
+  set axisWeight(value) {
     if (this._axisWeight === value) return;
-
     this._axisWeight = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * majorTickAlpha
    */
-  get majorTickAlpha(): number {
+  get majorTickAlpha() {
     return this._majorTickAlpha;
   }
 
-  set majorTickAlpha(value: number) {
+  set majorTickAlpha(value) {
     if (this._majorTickAlpha === value) return;
-
     this._majorTickAlpha = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * majorTickColor
    */
-  get majorTickColor(): number {
+  get majorTickColor() {
     return this._majorTickColor;
   }
 
-  set majorTickColor(value: number) {
+  set majorTickColor(value) {
     if (this._majorTickColor === value) return;
-
     this._majorTickColor = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * majorTickCount
    */
-  get majorTickCount(): number {
+  get majorTickCount() {
     return this._majorTickCount;
   }
 
-  set majorTickCount(value: number) {
+  set majorTickCount(value) {
     if (this._majorTickCount === value) return;
-
     this._majorTickCount = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * majorTickLength
    */
-  get majorTickLength(): number {
+  get majorTickLength() {
     return this._majorTickLength;
   }
 
-  set majorTickLength(value: number) {
+  set majorTickLength(value) {
     if (this._majorTickLength === value) return;
-
     this._majorTickLength = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * majorTickWeight
    */
-  get majorTickWeight(): number {
+  get majorTickWeight() {
     return this._majorTickWeight;
   }
 
-  set majorTickWeight(value: number) {
+  set majorTickWeight(value) {
     if (this._majorTickWeight === value) return;
-
     this._majorTickWeight = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * maximum
    */
-  get maximum(): number {
+  get maximum() {
     return this._maximum;
   }
 
-  set maximum(value: number) {
+  set maximum(value) {
     if (this._maximum === value) return;
-
     this._maximum = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * minimum
    */
-  get minimum(): number {
+  get minimum() {
     return this._minimum;
   }
 
-  set minimum(value: number) {
+  set minimum(value) {
     if (this._minimum === value) return;
-
     this._minimum = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * minorTickAlpha
    */
-  get minorTickAlpha(): number {
+  get minorTickAlpha() {
     return this._minorTickAlpha;
   }
 
-  set minorTickAlpha(value: number) {
+  set minorTickAlpha(value) {
     if (this._minorTickAlpha === value) return;
-
     this._minorTickAlpha = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * minorTickColor
    */
-  get minorTickColor(): number {
+  get minorTickColor() {
     return this._minorTickColor;
   }
 
-  set minorTickColor(value: number) {
+  set minorTickColor(value) {
     if (this._minorTickColor === value) return;
-
     this._minorTickColor = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * minorTickCount
    */
-  get minorTickCount(): number {
+  get minorTickCount() {
     return this._minorTickCount;
   }
 
-  set minorTickCount(value: number) {
+  set minorTickCount(value) {
     if (this._minorTickCount === value) return;
-
     this._minorTickCount = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * minorTickLength
    */
-  get minorTickLength(): number {
+  get minorTickLength() {
     return this._minorTickLength;
   }
 
-  set minorTickLength(value: number) {
+  set minorTickLength(value) {
     if (this._minorTickLength === value) return;
-
     this._minorTickLength = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * minorTickWeight
    */
-  get minorTickWeight(): number {
+  get minorTickWeight() {
     return this._minorTickWeight;
   }
 
-  set minorTickWeight(value: number) {
+  set minorTickWeight(value) {
     if (this._minorTickWeight === value) return;
-
     this._minorTickWeight = value;
-    this._dirty = true;
+    this.invalidate();
   }
 
   /**
    * precision
    */
-  get precision(): number {
+  get precision() {
     return this._precision;
   }
 
-  set precision(value: number) {
+  set precision(value) {
     if (this._precision === value) return;
-
     this._precision = value;
-    this._dirty = true;
+    this.invalidate();
   }
-
-  //------------------------------
-  //  methods
-  //------------------------------
-
-  abstract renderAxis(): void;
-  abstract renderLabels(): void;
-  abstract renderTickMarks(): void;
 }
