beeb99c410cfd6cbfacda506828b91247b3fcdb6
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:19:05 -0500

    Polar axis default options

---
+++ src/factory/PolarAxisDefaultOptions.ts
@@ -0,0 +1,9 @@
+import { AxisDefaultOptions } from "./AxisDefaultOptions";
+import { PolarAxisOptions } from "../display/PolarAxisOptions";
+
+export const PolarAxisDefaultOptions: PolarAxisOptions = {
+  ...AxisDefaultOptions,
+  radius: 100,
+  spanAngle: 260,
+  startAngle: 140,
+};
