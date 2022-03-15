f5b9501211ead59000a1d6c571879489a9895d69
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:18:42 -0500

    Needle default options

---
+++ src/factory/NeedleDefaultOptions.ts
@@ -0,0 +1,12 @@
+import { NeedleOptions } from "../display/NeedleOptions";
+
+export const NeedleDefaultOptions: NeedleOptions = {
+  needleBaseWidth: 4,
+  needleFillAlpha: 1.0,
+  needleFillColor: 0x313131,
+  needleRadius: 90,
+  needleStrokeAlpha: 1.0,
+  needleStrokeColor: 0xffffff,
+  needleStrokeWeight: 1,
+  needleTipWidth: 1,
+};
