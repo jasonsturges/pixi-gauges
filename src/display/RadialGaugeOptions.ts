00d1c1279575b53d2f6a6d2acdb140092c7e9b6a
Jason Sturges <jason@jasonsturges.com>
2022-03-14 23:48:24 -0500

    Radial gauge options

--- src/display/RadialGaugeOptions.ts
+++ src/display/RadialGaugeOptions.ts
@@ -1,40 +1,9 @@
+import { AxisOptions } from "./AxisOptions";
+import { GaugeOptions } from "./GaugeOptions";
+import { NeedleOptions } from "./NeedleOptions";
+import { PolarAxisOptions } from "./PolarAxisOptions";
+
 /**
  * Radial Gauge optional parameters
  */
-export type RadialGaugeOptions = {
-  autoAdjustMinMax: boolean;
-  axisAlpha: number;
-  axisColor: number;
-  axisLabelFontAlpha: number;
-  axisLabelFontColor: number;
-  axisLabelFontName: string;
-  axisLabelFontSize: number;
-  axisLabelGap: number;
-  axisLabelsVisible: boolean;
-  axisVisible: boolean;
-  axisWeight: number;
-  majorTickAlpha: number;
-  majorTickColor: number;
-  majorTickCount: number;
-  majorTickLength: number;
-  majorTickWeight: number;
-  maximum: number;
-  minimum: number;
-  minorTickAlpha: number;
-  minorTickColor: number;
-  minorTickCount: number;
-  minorTickLength: number;
-  minorTickWeight: number;
-  needleBaseWidth: number;
-  needleFillAlpha: number;
-  needleFillColor: number;
-  needleRadius: number;
-  needleStrokeAlpha: number;
-  needleStrokeColor: number;
-  needleStrokeWeight: number;
-  needleTipWidth: number;
-  precision: number;
-  radius: number;
-  spanAngle: number;
-  startAngle: number;
-};
+export type RadialGaugeOptions = AxisOptions & GaugeOptions & NeedleOptions & PolarAxisOptions;
